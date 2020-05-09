c238c7cc63eac92a08ca651ccff473e8c576767a
sergei_vydra <vydrasergei@gmail.com>
2020-05-09 15:23:30 +0300

    formatted for review

--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,7 @@ class TestUserAddToCartFromProductPage(object):
         login_page.should_be_logged_in()
 
     @pytest.mark.need_review
-    def test_user_can_add_product_to_cart(self, browser):
+    def test_user_can_add_product_to_busket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         page.open()
@@ -39,13 +39,32 @@ class TestUserAddToCartFromProductPage(object):
 
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_cart(browser):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_cart_link()
     page.add_product_to_cart()
     page.check_info(link)
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_not_be_items_in_cart()
+    cart_page.should_be_text_cart_is_empty()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 @pytest.mark.other
@@ -72,22 +91,3 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
-
-@pytest.mark.need_review
-def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_cart_page()
-    cart_page = CartPage(browser, browser.current_url)
-    cart_page.should_not_be_items_in_cart()
-    cart_page.should_be_text_cart_is_empty()
